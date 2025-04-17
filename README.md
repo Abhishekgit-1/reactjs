f574663f7dac0b7b9c7f86f40d4b0260fe28c736
abhishek mathur <abhishekmathur2807@gmail.com>
2025-04-18 02:42:20 +0530

    episode 2

---
+++ README.md
@@ -0,0 +1,10 @@
+# React js 
+
+# Parcel
+-Dev Build
+-Local Server
+-HMR - Hot Module Replacement
+-File Watching Algorithm - Written in C++
+-minification
+-Catching - build fast
+Image Optimization
--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-
-    //**     // const heading = React.createElement('h1', {
-        //     id: "heading",
-        //     xyz : "abc",
-        // }, 'Hello World!');
-        // const root = ReactDOM.createRoot(document.getElementById('root'));
-        // root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
    
 
-
+//** 
 // <div id="parent">
 //      <div id="child1">
 //          <h1>I am heading</h1>
@@ -24,18 +19,18 @@ const parent = React.createElement("div",
     {id: "parent"},
      [React.createElement(
         "div", 
-        {id: "child1"},
-        [React.createElement("h1", { id: "h1"}, "I am h1 tag"),
-        React.createElement("h2", { id: "h2"}, "I am h2 tag"),
-        React.createElement("h3", { id: "h3"}, "I am h3 tag")]
+        {id: "child1", key: "child12"},
+        [React.createElement("h1", { id: "h1", key :"1"}, "I am h1 tag  test"),
+        React.createElement("h2", { id: "h2", key :"2"}, "I am h2 tag"),
+        React.createElement("h3", { id: "h3",  key :"3"}, "I am h3 tag")]
     ),React.createElement(
         "div", 
-        {id: "child2"},
-        [React.createElement("h1", { id: "h1"}, "I am h1 tag"),
-        React.createElement("h2", { id: "h2"}, "I am h2 tag"),
-        React.createElement("h3", { id: "h3"}, "I am h3 tag")]
+        {id: "child2", key: "child13"},
+        [React.createElement("h1", { id: "h4", key :"4"}, "I am h1 tag"),
+        React.createElement("h2", { id: "h5", key :"5"}, "I am h2 tag"),
+        React.createElement("h3", { id: "h6", key :"6"}, "I am h3 tag")]
     )]
 );
-console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
